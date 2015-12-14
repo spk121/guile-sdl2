ba5783e3bdc6a1bd3c53a2bb5605c605e9af9a26
David Thompson <dthompson2@worcester.edu>
2015-12-13 22:56:27 -0500

    Replace blank window example with hello example.

---
+++ examples/hello.scm
@@ -0,0 +1,37 @@
+;;; guile-sdl2 --- FFI bindings for SDL2
+;;; Copyright © 2015 David Thompson <davet@gnu.org>
+;;;
+;;; This file is part of guile-sdl2.
+;;;
+;;; Guile-sdl2 is free software; you can redistribute it and/or modify
+;;; it under the terms of the GNU Lesser General Public License as
+;;; published by the Free Software Foundation; either version 3 of the
+;;; License, or (at your option) any later version.
+;;;
+;;; Guile-sdl2 is distributed in the hope that it will be useful, but
+;;; WITHOUT ANY WARRANTY; without even the implied warranty of
+;;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+;;; General Public License for more details.
+;;;
+;;; You should have received a copy of the GNU Lesser General Public
+;;; License along with guile-sdl2.  If not, see
+;;; <http://www.gnu.org/licenses/>.
+
+(use-modules (sdl2)
+             (sdl2 render)
+             (sdl2 surface)
+             (sdl2 video))
+
+(sdl-init)
+
+(define window (make-sdl-window))
+(define ren (make-renderer window))
+(define surface (load-bmp "hello.bmp"))
+(define texture (surface->texture ren surface))
+
+(clear-renderer ren)
+(render-copy ren texture)
+(present-renderer ren)
+(sleep 2)
+(close-sdl-window! window)
+(sdl-quit)
